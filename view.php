2b1cc4b348fd6368e12fd863864be8c01ee64741
calypso skinner <calypso.skinner@gmail.com>
2013-05-31 17:26:56 +0200

    all done

--- view.php
+++ view.php
@@ -10,20 +10,24 @@
 	
 	<body>
 		
-	<div id="header_nav">
 		<header>
-			<h1 class="bigNoodle">Loic Marques</h1>
+	<div id="header">
+			<h1 class="bigNoodle"><a href="#" title="homepage">Loic Marques</a></h1>
 			<h2 class="helvetica">2D/3D art</h2>
 			<img src="imgs/header_sep.png" alt="header separation" width="550" height="30" />
 			<h3 class="helvetica">loicmarques93@gmail.com</h3>
+		
+	</div>
 		</header>
 		
+		
+		<div id="nav_w">
 		<nav>
 			<div id="nav">
 				<ul>
 					<li><a href="#works" class="helvetica" title="anchor">Works</a></li>
 					<li><a href="#about" class="helvetica" title="anchor">About</a></li>
-					<li><a href="#home" class="helvetica liner" title="anchor">
+					<li><a href="#" class="helvetica liner" title="anchor">
 						<img src="imgs/home_icon.png" alt="home icon" />
 					</a></li>
 					<li><a href="#contact" class="helvetica" title="anchor">Contact</a></li>
@@ -31,37 +35,182 @@
 				</ul>
 			</div>
 		</nav>
+		</div>
 		
-	</div>
-	
-		<div id="home" class="hidden">&nbsp;
-		<div class="main_block">
+		<div id="stickyalias"></div>
+		
+		<div  class="main_block"><span id="home" class="anchor"> </span><span class="anchor"> </span>
 			<iframe src="http://player.vimeo.com/video/17535548" width="1200" height="500" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
-		</div></div>
+		</div>
 		
-		<div id="works" class="hidden">&nbsp;
-		<div class="main_block">
-			works
-		</div></div>
+		<div class="main_block"><span id="works" class="anchor"> </span>
+			<div class="img_block light rec_side">
+				<img src="imgs/mode.png" alt="work1" class="rec_side img_pop" />
+			</div>
+			<div class="popup_w">
+				<div id="mode" class="popup light" style="display:none;">
+					<div class="close_pop" title="mode">x</div>
+					<h2>MODE</h2>
+					<img src="imgs/mode.png" alt="work1" />
+					<p>
+						Bacon ipsum dolor sit amet leberkas beef ribs bacon hamburger. Andouille tail tongue venison. Ball tip venison doner ground round sausage filet mignon pastrami spare ribs beef ribs brisket pork loin jowl flank rump tongue. Sausage venison corned beef pork chop ribeye tenderloin spare ribs drumstick pork loin chicken short loin tail. Corned beef shankle drumstick sirloin tenderloin. Shoulder beef pancetta shankle capicola pork ham hamburger jerky cow.
+					</p>
+				</div>
+			</div>
+
+			<div class="img_block dark rec_up">
+				<img src="imgs/mode0.png" alt="work1" class="rec_up img_pop" />
+			</div>
+			<div class="popup_w">
+				<div id="mode0" class="popup dark" style="display:none;">
+					<div class="close_pop" title="mode0">x</div>
+					<h2>MODE 0</h2>
+					<img src="imgs/mode0.png" alt="work1" />
+					<p>
+						Bacon ipsum dolor sit amet leberkas beef ribs bacon hamburger. Andouille tail tongue venison. Ball tip venison doner ground round sausage filet mignon pastrami spare ribs beef ribs brisket pork loin jowl flank rump tongue. Sausage venison corned beef pork chop ribeye tenderloin spare ribs drumstick pork loin chicken short loin tail. Corned beef shankle drumstick sirloin tenderloin. Shoulder beef pancetta shankle capicola pork ham hamburger jerky cow.
+					</p>
+				</div>
+			</div>
+			
+			<div class="img_block light rec_up">
+				<img src="imgs/mode1.png" alt="work1" class="rec_up img_pop" />
+			</div>
+			<div class="popup_w">
+				<div id="mode1" class="popup light" style="display:none;">
+					<div class="close_pop" title="mode1">x</div>
+					<h2>MODE 1</h2>
+					<img src="imgs/mode1.png" alt="work1" />
+					<p>
+						Bacon ipsum dolor sit amet leberkas beef ribs bacon hamburger. Andouille tail tongue venison. Ball tip venison doner ground round sausage filet mignon pastrami spare ribs beef ribs brisket pork loin jowl flank rump tongue. Sausage venison corned beef pork chop ribeye tenderloin spare ribs drumstick pork loin chicken short loin tail. Corned beef shankle drumstick sirloin tenderloin. Shoulder beef pancetta shankle capicola pork ham hamburger jerky cow.
+					</p>
+				</div>
+			</div>
+			
+			<div class="img_block dark rec_side">
+				<img src="imgs/mode2.png" alt="work1" class="rec_side img_pop" />
+			</div>
+			<div class="popup_w">
+				<div id="mode2" class="popup dark" style="display:none;">
+					<div class="close_pop" title="mode2">x</div>
+					<h2>MODE 2</h2>
+					<img src="imgs/mode2.png" alt="work1" />
+					<p>
+						Bacon ipsum dolor sit amet leberkas beef ribs bacon hamburger. Andouille tail tongue venison. Ball tip venison doner ground round sausage filet mignon pastrami spare ribs beef ribs brisket pork loin jowl flank rump tongue. Sausage venison corned beef pork chop ribeye tenderloin spare ribs drumstick pork loin chicken short loin tail. Corned beef shankle drumstick sirloin tenderloin. Shoulder beef pancetta shankle capicola pork ham hamburger jerky cow.
+					</p>
+				</div>
+			</div>
+		</div>
 		
-		<div id="about" class="hidden">&nbsp;
-		<div class="main_block">
-			about
-		</div></div>
+		<div class="text_block main_block"><span id="about" class="anchor"> </span>
+			<h2 class="bigNoodle">WHO AM I?</h2>
+			<p class="helvetica">
+				Bacon ipsum dolor sit amet leberkas beef ribs bacon hamburger. Andouille tail tongue venison. Ball tip venison doner ground round sausage filet mignon pastrami spare ribs beef ribs brisket pork loin jowl flank rump tongue. Sausage venison corned beef pork chop ribeye tenderloin spare ribs drumstick pork loin chicken short loin tail. Corned beef shankle drumstick sirloin tenderloin. Shoulder beef pancetta shankle capicola pork ham hamburger jerky cow.
+				Bacon ipsum dolor sit amet leberkas beef ribs bacon hamburger. Andouille tail tongue venison. Ball tip venison doner ground round sausage filet mignon pastrami spare ribs beef ribs brisket pork loin jowl flank rump tongue. Sausage venison corned beef pork chop ribeye tenderloin spare ribs drumstick pork loin chicken short loin tail. Corned beef shankle drumstick sirloin tenderloin. Shoulder beef pancetta shankle capicola pork ham hamburger jerky cow.
+				Bacon ipsum dolor sit amet leberkas beef ribs bacon hamburger. Andouille tail tongue venison. Ball tip venison doner ground round sausage filet mignon pastrami spare ribs beef ribs brisket pork loin jowl flank rump tongue. Sausage venison corned beef pork chop ribeye tenderloin spare ribs drumstick pork loin chicken short loin tail. Corned beef shankle drumstick sirloin tenderloin. Shoulder beef pancetta shankle capicola pork ham hamburger jerky cow.
+				Bacon ipsum dolor sit amet leberkas beef ribs bacon hamburger. Andouille tail tongue venison. Ball tip venison doner ground round sausage filet mignon pastrami spare ribs beef ribs brisket pork loin jowl flank rump tongue. Sausage venison corned beef pork chop ribeye tenderloin spare ribs drumstick pork loin chicken short loin tail. Corned beef shankle drumstick sirloin tenderloin. Shoulder beef pancetta shankle capicola pork ham hamburger jerky cow.
+			</p>
+			<h3><a href="#" target="_blank" class="bigNoodle">DOWNLOAD CV</a></h3>
+		</div>
 		
-		<div id="contact" class="hidden">&nbsp;
-		<div class="main_block">
-			contact
-		</div></div>
+		<div  class="text_block min_text main_block"><span id="contact" class="anchor"> </span>
+			<h2 class="data_contact helvetica">Contact Info</h2>
+			<h4 class="bigNoodle">IF YOU WOULD LIKE TO GET IN TOUCH WITH ME:</h4>
+			<h3 class="contact_email bigNoodle">LOICMARQUES@GMAIL.COM</h3>
+		</div>
 		
-		<div id="links" class="hidden">&nbsp;
-		<div class="main_block">
-			links
-		</div></div>
+		<div class="text_block mid_block main_block"><span id="links" class="anchor"> </span>
+			<h2 class="helvetica links_title">Links</h2>
+			<div class="midle">
+				<div class="left">
+					<div class="link">
+						<a href="#" target="_blank" title="cg society">
+							<img class="left" src="imgs/link0.png" alt="link0" width="32" height="32" />
+							<p class="bigNoodle left">CG SOCIETY</p>
+						</a>
+					</div>
+					<div class="link">
+						<a href="#" target="_blank" title="behance">
+							<img class="left" src="imgs/link1.png" alt="link1" width="32" height="32" />
+							<p class="bigNoodle left">BEHANCE</p>
+						</a>
+					</div>
+					<div class="link">
+						<a href="#" target="_blank" title="youtube">
+							<img class="left" src="imgs/link2.png" alt="link2" width="32" height="32" />
+							<p class="bigNoodle left">YOUTUBE</p>
+						</a>
+					</div>
+				</div>
+				
+				<div class="left">
+					<div class="link">
+						<a href="#" target="_blank" title="cg hub">
+							<img class="left" src="imgs/link3.png" alt="link3" width="32" height="32" />
+							<p class="bigNoodle left">CG HUB</p>
+						</a>
+					</div>
+					<div class="link">
+						<a href="#" target="_blank" title="dribble">
+							<img class="left" src="imgs/link4.png" alt="link4" width="32" height="32" />
+							<p class="bigNoodle left">DRIBBLE</p>
+						</a>
+					</div>
+					<div class="link">
+						<a href="#" target="_blank" title="twitter">
+							<img class="left" src="imgs/link5.png" alt="link5" width="32" height="32" />
+							<p class="bigNoodle left">TWITTER</p>
+						</a>
+					</div>
+				</div>
+			
+				<div class="left">
+					<div class="link">
+						<a href="#" target="_blank" title="linked in">
+							<img class="left" src="imgs/link6.png" alt="link6" width="32" height="32" />
+							<p class="bigNoodle left">LINKED IN</p>
+						</a>
+					</div>
+					<div class="link">
+						<a href="#" target="_blank" title="deviantart">
+							<img class="left" src="imgs/link7.png" alt="link7" width="32" height="32" />
+							<p class="bigNoodle left">DEVIANTART</p>
+						</a>
+					</div>
+					<div class="link">
+						<a href="#" target="_blank" title="facebook">
+							<img class="left" src="imgs/link8.png" alt="link8" width="32" height="32" />
+							<p class="bigNoodle left">FACEBOOK</p>
+						</a>
+					</div>
+				</div>
+				
+				<div class="left">
+					<div class="link">
+						<a href="#" target="_blank" title="gmail">
+							<img class="left" src="imgs/link9.png" alt="link9" width="32" height="32" />
+							<p class="bigNoodle left">GMAIL</p>
+						</a>
+					</div>
+					<div class="link">
+						<a href="#" target="_blank" title="pinterest">
+							<img class="left" src="imgs/link10.png" alt="link10" width="32" height="32" />
+							<p class="bigNoodle left">PINTEREST</p>
+						</a>
+					</div>
+					<div class="link">
+						<a href="#" title="skype">
+							<img class="left" src="imgs/link11.png" alt="link11" width="32" height="32" />
+							<p class="bigNoodle left">SKYPE</p>
+						</a>
+					</div>
+				</div>
+			<div class="clear"> </div>
+			</div>
+		</div>
 		
 		<footer>
 			<div id="footer">
-				&nbsp;
+				<p class="bigNoodle"><a href="#" title="homepage">LOIC MARQUES - 2013</a></p>
 			</div>
 		</footer>
 	</body>
@@ -77,6 +226,38 @@
 				$('#nav li a img').attr("src","imgs/home_icon.png");
 			}
 		);
+		
+		$("img.img_pop").click(function() {
+			var src= this.src;
+			var img= src.split('imgs/')[1];
+			var id= img.split('.')[0];
+			var id= '#'+id;
+			$(id).css('margin-top', "-500px");
+			$(id).show();
+			
+		});
+		
+		$(".close_pop").click(function() {
+			var id= this.title;
+			var id = '#'+id;
+			$(id).hide();
+		});
+		
+		
+		
+		$(function(){
+        	// Check the initial Poistion of the Sticky Header
+        	var stickyHeaderTop = $('#nav_w').offset().top;
+        	$(window).scroll(function(){
+                if( $(window).scrollTop() > stickyHeaderTop ) {
+                        $('#nav_w').css({position: 'fixed', top: '0px'});
+                        $('#stickyalias').css('display', 'block');
+                } else {
+                        $('#nav_w').css({position: 'static', top: '0px'});
+                        $('#stickyalias').css('display', 'none');
+                }
+    	    });
+  		});
 		
 		
 	</script>
