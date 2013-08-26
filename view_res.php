e81366c6e46a4e6c0c4bdb2f8168ccb80f29f47e
calypso skinner <calypso.skinner@gmail.com>
2013-08-26 15:59:22 +0200

    bg

--- view_res.php
+++ view_res.php
@@ -11,8 +11,8 @@
 	<body>
 		<header>
 	<div id="header">
-			<h1 class="bigNoodle"><a href="#" title="homepage">Loic Marques</a></h1>
-			<h2 class="helvetica">2D/3D art</h2>
+			<img src="imgs/logo.png" width="400" height="100" />
+			<h2 class="helvetica">Character Artist</h2>
 			<img src="imgs/header_sep.png" alt="header separation" width="550" height="30" />
 			<h3 class="helvetica"><a class="mailto" href="mailto:loicmarques93@gmail.com?subject=Recrut_website">loicmarques93@gmail.com</a></h3>
 		
