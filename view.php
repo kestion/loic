f3c7b997e908e6306a3d733f1fa6f3a455f71847
kestion <calypso.skinner@gmail.com>
2014-06-26 18:04:04 +0200

    simpler site 26-06-14

--- view.php
+++ view.php
@@ -1,4 +1,4 @@
-<!DOCTYPE  html>
+<!DOCTYPE html>
 
 <html>
 	
@@ -22,43 +22,38 @@
 		<header>
 			<div id="header">
 				<img src="imgs/logo.png" width="400" height="100" />
-				<h2 class="helvetica">Character Artist</h2>
+				<h2 class="luzro">Character Artist</h2>
 				<img src="imgs/header_sep.jpg" alt="header separation" width="550" height="30" />
-				<h3 class="helvetica"><a class="mailto" href="mailto:loicmarques93@gmail.com?subject=Recrut_website">loicmarques93@gmail.com</a></h3>		
+				<h3 class="luzro"><a class="mailto" href="mailto:loicmarques93@gmail.com?subject=Recrut_website">loicmarques93@gmail.com</a></h3>		
 			</div>
 		</header>
 		
 		<div id="nav_w">
-		<nav>
-			<div id="nav">
-				<ul>
-					<li><a href="#contact" class="helvetica" title="anchor">Contact</a></li>
-					<li><a href="#" class="helvetica liner" title="anchor">
-						<img src="imgs/home_icon.png" alt="home icon" />
-					</a></li>
-					<li><a href="#about" class="helvetica" title="anchor">About</a></li>
-				</ul>
-			</div>
-		</nav>
+			<nav>
+				<div id="nav">
+					<ul>
+						<li><a class="luzro" href="mailto:loicmarques93@gmail.com?subject=Recrut_website" title="mailto">Contact</a></li>
+						<li><a href="#" class="luzro liner" title="anchor">
+							<img src="imgs/home_icon.png" alt="home icon" />
+						</a></li>
+						<li><a href="cv.pdf" target="_blank" class="luzro" title="resume">Resume</a></li>
+					</ul>
+				</div>
+			</nav>
 		</div>
 		
 		<div id="stickyalias"></div>
 	
-		
-		<div class="main_block"><span id="home" class="anchor"> </span>
-			
+		<div class="main_block"><span id="home" class="anchor"> </span>	
 			<!--<div class="title_proj">
 				Alien The Intruder[Concept]
 			</div>-->
-
-            <div class="img_block light rec_side">
+			<div class="img_block rec_side">
 				<img src="imgs/17.jpg" alt="alien intruder" class="rec_side img_pop" />
 			</div>
-			
-            <div class="img_block light rec_side">
+            <div class="img_block rec_side">
 				<img src="imgs/16.jpg" alt="alien intruder" class="rec_side img_pop" />
 			</div>
-		
 			<div style="margin-bottom:25px; padding:50px;">
 				<img src="imgs/sep.jpg" alt="header separation" width="550" height="30" />
 			</div>
@@ -66,47 +61,18 @@
 			<!--<div class="title_proj">
 				Oro [Fighting Games Zbrush Challenge]
 			</div>-->
-			
-			 <div class="img_block light rec_side">
+			 <div class="img_block rec_side">
 				<img src="imgs/15.jpg" alt="oro" class="rec_side img_pop" />
 			</div>
-			
-			 <div class="img_block light rec_side">
+			 <div class="img_block rec_side">
 				<img src="imgs/14.jpg" alt="oro" class="rec_side img_pop" />
 			</div>
-			
-				<div class="img_block light rec_side">
+			<div class="img_block rec_side">
 				<img src="imgs/13.jpg" alt="oro" class="rec_side img_pop" />
-			</div>
-			
-			<div class="img_block light rec_side">
+			</div>			
+			<div class="img_block rec_side">
 				<img src="imgs/12.jpg" alt="oro" class="rec_side img_pop" />
 			</div>
-			
-			<div style="margin-bottom:25px; padding:50px;">
-				<img src="imgs/sep.jpg" alt="header separation" width="550" height="30" />
-			</div>
-			
-				<!--<div class="title_proj">
-				Franky [Character Design]
-			</div>-->
-			
-			<div class="img_block light rec_side">
-				<img src="imgs/11.jpg" alt="franky" class="rec_side img_pop" />
-			</div>
-			
-			<div class="img_block dark rec_side">
-				<img src="imgs/10.jpg" alt="franky" class="rec_side img_pop" />
-			</div>
-			
-			<div class="img_block light rec_side">
-				<img src="imgs/09.jpg" alt="franky" class="rec_side img_pop" />
-			</div>
-			
-			<div class="img_block dark rec_side">
-				<img src="imgs/08.jpg" alt="franky" class="rec_side img_pop" />
-			</div>
-			
 			<div style="margin-bottom:25px; padding:50px;">
 				<img src="imgs/sep.jpg" alt="header separation" width="550" height="30" />
 			</div>
@@ -114,20 +80,15 @@
 			<!--<div class="title_proj">
 				Kerrigan
 			</div>-->
-			
-			<div class="img_block light rec_side">
+			<div class="img_block rec_side">
 				<img src="imgs/07.jpg" alt="kerrigan" class="rec_side img_pop" />
 			</div>
-			
-			<div class="img_block dark rec_side">
+			<div class="img_block rec_side">
 				<img src="imgs/06.jpg" alt="kerrigan" class="rec_side img_pop" />
-			</div>
-			
-			<div class="img_block dark rec_side">
+			</div>	
+			<div class="img_block rec_side">
 				<img src="imgs/05.jpg" alt="kerrigan" class="rec_side img_pop" />
 			</div>
-			
-		
 		    <div style="margin-bottom:25px; padding:50px;">
 				<img src="imgs/sep.jpg" alt="header separation" width="550" height="30" />
 			</div>
@@ -135,15 +96,12 @@
 			<!--<div class="title_proj">
 				Roboto [Character Design]
 			</div>-->
-			
-			<div class="img_block light rec_side">
+			<div class="img_block rec_side">
 				<img src="imgs/04.jpg" alt="blodia" class="rec_side img_pop" />
 			</div>
-			
-			<div class="img_block light rec_side">
+			<div class="img_block rec_side">
 				<img src="imgs/03.jpg" alt="blodia" class="rec_side img_pop" />
 			</div>
-			
 			<div style="margin-bottom:25px; padding:50px;">
 				<img src="imgs/sep.jpg" alt="header separation" width="550" height="30" />
 			</div>
@@ -151,132 +109,24 @@
 			<!--<div class="title_proj">
 				Alien[WIP]
 			</div>-->
-			<div class="img_block dark rec_side">
+			<div class="img_block rec_side">
 				<img src="imgs/02.jpg" alt="alien prince" class="rec_side img_pop" />
 			</div>
-			
-			<div class="img_block dark rec_side">
+			<div class="img_block rec_side">
 				<img src="imgs/01.jpg" alt="alien prince" class="rec_side img_pop" />
 			</div>
-			
-			<div class="img_block light rec_side">
+			<div class="img_block rec_side">
 				<img src="imgs/00.jpg" alt="alien prince" class="rec_side img_pop" />
 			</div>
-			
 		</div>
+
+		<div class="clear"> </div>
 		
-		<div style="margin-top:150px;">
-			<center><img src="imgs/sep.jpg" alt="header separation" width="550" height="30" /></center>
+		<div id="top_anchor">
+			<a href="#"><img src="imgs/top.png" alt="top" /></a>
+			<a href="#" class="luzro">TOP</a>
 		</div>
-		
-		<div id="contact_div" class="topping text_block min_text main_block"><span id="contact" class="anchor"> </span>
-			<h2 class="data_contact helvetica">Contact Info</h2>
-			<h4 class="bigNoodle">IF YOU WOULD LIKE TO GET IN TOUCH WITH ME:</h4>
-			<h3 class="contact_email bigNoodle"><a class="mailto" href="mailto:loicmarques93@gmail.com?subject=Recrut_website">LOICMARQUES93@GMAIL.COM</a></h3>
-		</div>
-		
-		<div style="margin-bottom:125px;">
-			<center><img src="imgs/sep.jpg" alt="header separation" width="550" height="30" /></center>
-		</div>
-		
-		<div id="about_div" class="text_block main_block mid_block but_long"><span id="about" class="anchor"> </span>
-			<h2 class="bigNoodle">WHO AM I?</h2>
-			<p class="helvetica">
-				Born in 1990 and raised in the suburbs of Paris, I graduated from school in 2013. I'm now specializing in character, game modeling, digital sculpting, and texturing. I am most proficient with Zbrush and Photoshop but i'm very versatile and can do a lot of differents things: Web/Interface/2D or even Sound Design. I love to give shape to concepts, and I've always had a passion for characters in games and movies. I am eager to improve more and more as an artist; to always add more subtleties and life to my models. 
-			</p>
-			<h3><a href="cv.pdf" target="_blank" class="bigNoodle">DOWNLOAD CV</a></h3>
-		</div>
-		
-		<!--<div class="text_block mid_block main_block"><span id="links" class="anchor"> </span>
-			<h2 class="helvetica links_title">Links</h2>
-			<div class="midle">
-				<div class="left">
-					<div class="link">
-						<a href="#" target="_blank" title="cg society">
-							<img class="left" src="imgs/link0.png" alt="link0" width="32" height="32" />
-							<p class="bigNoodle left">CG SOCIETY</p>
-						</a>
-					</div>
-					<div class="link">
-						<a href="#" target="_blank" title="behance">
-							<img class="left" src="imgs/link1.png" alt="link1" width="32" height="32" />
-							<p class="bigNoodle left">BEHANCE</p>
-						</a>
-					</div>
-					<div class="link">
-						<a href="#" target="_blank" title="youtube">
-							<img class="left" src="imgs/link2.png" alt="link2" width="32" height="32" />
-							<p class="bigNoodle left">YOUTUBE</p>
-						</a>
-					</div>
-				</div>
-				
-				<div class="left">
-					<div class="link">
-						<a href="#" target="_blank" title="cg hub">
-							<img class="left" src="imgs/link3.png" alt="link3" width="32" height="32" />
-							<p class="bigNoodle left">CG HUB</p>
-						</a>
-					</div>
-					<div class="link">
-						<a href="#" target="_blank" title="dribble">
-							<img class="left" src="imgs/link4.png" alt="link4" width="32" height="32" />
-							<p class="bigNoodle left">DRIBBLE</p>
-						</a>
-					</div>
-					<div class="link">
-						<a href="#" target="_blank" title="twitter">
-							<img class="left" src="imgs/link5.png" alt="link5" width="32" height="32" />
-							<p class="bigNoodle left">TWITTER</p>
-						</a>
-					</div>
-				</div>
-			
-				<div class="left">
-					<div class="link">
-						<a href="#" target="_blank" title="linked in">
-							<img class="left" src="imgs/link6.png" alt="link6" width="32" height="32" />
-							<p class="bigNoodle left">LINKED IN</p>
-						</a>
-					</div>
-					<div class="link">
-						<a href="#" target="_blank" title="deviantart">
-							<img class="left" src="imgs/link7.png" alt="link7" width="32" height="32" />
-							<p class="bigNoodle left">DEVIANTART</p>
-						</a>
-					</div>
-					<div class="link">
-						<a href="#" target="_blank" title="facebook">
-							<img class="left" src="imgs/link8.png" alt="link8" width="32" height="32" />
-							<p class="bigNoodle left">FACEBOOK</p>
-						</a>
-					</div>
-				</div>
-				
-				<div class="left">
-					<div class="link">
-						<a href="#" target="_blank" title="gmail">
-							<img class="left" src="imgs/link9.png" alt="link9" width="32" height="32" />
-							<p class="bigNoodle left">GMAIL</p>
-						</a>
-					</div>
-					<div class="link">
-						<a href="#" target="_blank" title="pinterest">
-							<img class="left" src="imgs/link10.png" alt="link10" width="32" height="32" />
-							<p class="bigNoodle left">PINTEREST</p>
-						</a>
-					</div>
-					<div class="link">
-						<a href="#" title="skype">
-							<img class="left" src="imgs/link11.png" alt="link11" width="32" height="32" />
-							<p class="bigNoodle left">SKYPE</p>
-						</a>
-					</div>
-				</div>-->
-			<div class="clear"> </div>
-			</div>
-		</div>
-		
+
 		<footer>
 			<div id="footer">
 				<p class="bigNoodle"><a href="#" title="homepage">LOIC MARQUES - 2013</a></p>
@@ -286,53 +136,14 @@
 	
 	<script src="jquery.js"></script>
 	<script>
-		
-		
-		$('#nav li a').click(
-			function() {
-				var href = $(this).attr('href');
-				$('#nav li a').css('border-bottom', '0');
-				$('#nav li a').css('color', '#fff');
-				$('#nav li a img').attr("src","imgs/home_icon.png");
-				$(this).css('border-bottom', '1px solid #ff3900');
-				$(this).css('color', '#848484');
-				if(href == '#about')
-				{
-					$('#about_div').css('border', '1px solid #ff3900');
-				}
-				if(href == '#contact')
-				{
-					$('#contact_div').css('border', '1px solid #ff3900');
-				}
-				if(href == '#')
-				{
-					$(this).children("img").attr('src', 'imgs/home_icon_hover.png!important');
-				}
-		});
-		
 		$('#nav li a img').hover(
 			function() {
-				$(this).attr("src","imgs/home_icon_hover.png");
+				if($(this).attr("src") == "imgs/home_icon.png")			
+					$(this).attr("src","imgs/home_icon_hover.png");
+				else
+					$(this).attr("src","imgs/home_icon.png");
 			}
 		);
-		/*
-		$("img.img_pop").click(function() {
-			var src= this.src;
-			var img= src.split('imgs/')[1];
-			var id= img.split('.')[0];
-			var id= '#'+id;
-			$(id).css('margin-top', "-500px");
-			$(id).show();
-			
-		});
-		
-		$(".close_pop").click(function() {
-			var id= this.title;
-			var id = '#'+id;
-			$(id).hide();
-		});*/
-		
-		
 		
 		$(function(){
         	// Check the initial Poistion of the Sticky Header
@@ -347,8 +158,5 @@
                 }
     	    });
   		});
-		
-		
 	</script>
-	
 </html>
